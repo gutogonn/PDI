a55471de3f469e52ca165828b2d866bfe8d96b18
Augusto Martins <augustogonn@gmail.com>
2020-04-14 16:22:55 -0300

    arrumado o grayscale

--- Assets/Scripts/Controller/ImageController.cs
+++ Assets/Scripts/Controller/ImageController.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class ImageController : MonoBehaviour
 {
     public Color data;
-
+    public SpriteRenderer image;
+    public Sprite newSprite;
     public Text rText, gText, bText;
     public Button grayscaleButton;
 
@@ -12,13 +14,14 @@ public class ImageController : MonoBehaviour
 
     public void Start()
     {
-        color = gameObject.GetComponent<SpriteRenderer>().sprite.texture.GetPixels();
+        color = image.sprite.texture.GetPixels();
         grayscaleButton.onClick.AddListener(turnGray);
+        //GetComponent<SpriteRenderer>().sprite = newSprite;
     }
 
     public void Update()
     {
-        PixelToMouse.Instance().GetSpritePixelColorUnderMousePointer(gameObject.GetComponent<SpriteRenderer>(), out data);
+        PixelToMouse.Instance().GetSpritePixelColorUnderMousePointer(image, out data);
         rText.text = data.r.ToString();
         gText.text = data.g.ToString();
         bText.text = data.b.ToString();
@@ -26,17 +29,24 @@ public class ImageController : MonoBehaviour
 
     public void turnGray()
     {
-        Texture2D texture = gameObject.GetComponent<SpriteRenderer>().sprite.texture;
-        for (int y = 0; y < texture.height; y++)
-        {
-            for (int x = 0; x < texture.width; x++)
-            {
-                Color pixel = texture.GetPixel(x,y);
+        newSprite = image.sprite;
+        Texture2D newImage = new Texture2D(newSprite.texture.width, newSprite.texture.height);
 
+        Texture2D itemBGTex = newSprite.texture;
+
+        for (int y = 0; y < newImage.height; y++)
+        {
+            for (int x = 0; x < newImage.width; x++)
+            {
+                Color pixel = newSprite.texture.GetPixel(x,y);
                 Color color = new Color((pixel.r + pixel.g + pixel.b)/3, (pixel.r + pixel.g + pixel.b) / 3, (pixel.r + pixel.g + pixel.b) / 3, 1);
-                texture.SetPixel(x, y, color);
+                newImage.SetPixel(x, y, color);
             }
         }
-        texture.Apply();
+        newImage.name = image.name + " - GrayScale";
+        newImage.Apply();
+        byte[] itemBGBytes = newImage.EncodeToPNG();
+        GetComponent<SpriteRenderer>().sprite = Sprite.Create(newImage, new Rect(0.0f, 0.0f, newImage.width, newImage.height), new Vector2(0.5f, 0.5f), 100.0f);
+        File.WriteAllBytes("Assets/Images/" + newSprite.name + " - GrayScale.png", itemBGBytes);
     }
 }
